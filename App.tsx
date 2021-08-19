c2dd7c2a43439fa268c867f82e93c807dad638aa
Paresh <aherparesh@gmail.com>
2021-08-19 23:03:56 +0530

    - Added home component - Added react native router dependencies

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Dimensions, StyleSheet, Text, View} from 'react-native';
+// import { NativeRouter, Route, Link } from "react-router-native";
 
+import Home from './src/Home/Home'
 const {height} = Dimensions.get('screen');
 
 const App = () => {
   return (
     <View style={styles.container}>
       <View style={styles.center}>
+        <Home/>
         <Text>Hello React Native Web!!!</Text>
       </View>
     </View>
-  );
+  );   
 };
 
 const styles = StyleSheet.create({
